650fb2d263aa3c76c397d86a162cd01c0b800207
Ian-Acosta2330 <ian2330acosta@gmail.com>
2025-08-27 08:42:53 -0600

    updated reame and client.jsx, imported react router, TODO crud

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter } from "react-router-dom";
+import Client from "./client";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Client />
+  </StrictMode>
+);
