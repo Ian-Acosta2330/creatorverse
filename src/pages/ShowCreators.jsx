4e6708fe6581789c037b6a2e0649aed92a73ec60
Ian-Acosta2330 <ian2330acosta@gmail.com>
2025-08-29 14:07:08 -0600

    show creators react file complete, simply

--- src/pages/ShowCreators.jsx
+++ src/pages/ShowCreators.jsx
@@ -1,7 +1,44 @@
+import { useState, useEffect } from "react";
+import { supabase } from "../client";
+
 function ShowCreators() {
+
+    const [all_creators, setCreators] = useState([]);
+    const [error,setError] = useState(null);
+
+    useEffect(() => {
+        const show_creators = async () => {
+            const { data, error } = await supabase
+                .from('creators')
+                .select()
+
+            if (error){
+                setError(error.message);
+            } else {
+                setCreators(data);
+                console.log("All Creators Displayed!");
+            }
+        };
+    show_creators();
+
+    }, []);
+
+    if (error) {
+        return <div>Error:{error}</div>;
+    }
+    
+
     return(
         <>
             <h1>Show Creators</h1>
+            <div>
+                <ul>
+                    {all_creators.map((creator)=>(
+                    <li key={creator.name}>{creator.name} | {creator.url} | {creator.description} | <img src={creator.imageURL}></img> <br></br><br></br></li>
+                    ))}
+                </ul>
+                
+            </div>
         </>
     );
 };
